1256de925d6a26788758ba4908895b7115bc72be
Vinayak266 <145597850+Vinayak266@users.noreply.github.com>
2023-09-26 21:36:25 +0530

    Add files via upload

---
+++ First Prog (1).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+vinayak
+
+
+# In[2]:
+
+
+1+3
+
+
+# In[4]:
+
+
+n= 4
+for i in range (1,11):
+   print(n,'x',i,'=',n*1)
+
+
+# In[ ]:
+
+
+
+
